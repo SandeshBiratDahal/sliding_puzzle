7c0822ee2e5c8677a3bae4be88cdeddc1c4e1615
Sandesh <sandeshdahal063@gmail.com>
2025-03-07 10:51:12 +0545

    Added the method to randomize the puzzle and check if it is solved or not

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame as pg, sys
+import pygame as pg, sys, random
 
 pg.init()
 
@@ -15,12 +15,13 @@ class Puzzle:
             for j in range(self.image_size[1] // self.cell_size)
         }
         self.individual_sprite_positions = [list(key) for key, item in self.individual_sprite.items()]
+        self.individual_sprite_positions.pop(0)
         self.remaining_piece = self.individual_sprite[(0, 0)]
         del self.individual_sprite[(0, 0)]
         self.current_empty_cell = (0, 0)
 
     def render(self, surf: pg.Surface): 
-        i = 1
+        i = 0
         for pos, sprite in (self.individual_sprite.items()):
             surf.blit(sprite, self.individual_sprite_positions[i])
             i += 1
@@ -56,38 +57,48 @@ class Puzzle:
         x, y = mouse_pos[0] // self.cell_size, mouse_pos[1] // self.cell_size
 
         if self.check_up(x, y):
-            i = 1
+            i = 0
             for pos, sprite in self.individual_sprite.items():
                 if self.individual_sprite_positions[i][0] // self.cell_size == x and self.individual_sprite_positions[i][1] // self.cell_size <= y and self.individual_sprite_positions[i][1] // self.cell_size > self.current_empty_cell[1]:
                     self.individual_sprite_positions[i][1] -= self.cell_size
                 i += 1
             self.current_empty_cell = x, y
         elif self.check_left(x, y):
-            i = 1
+            i = 0
             for pos, sprite in self.individual_sprite.items():
                 if self.individual_sprite_positions[i][1] // self.cell_size == y and self.individual_sprite_positions[i][0] // self.cell_size <= x and self.individual_sprite_positions[i][0] // self.cell_size > self.current_empty_cell[0]:
                     self.individual_sprite_positions[i][0] -= self.cell_size
                 i += 1
             self.current_empty_cell = x, y
         elif self.check_down(x, y):
-            i = 1
+            i = 0
             for pos, sprite in self.individual_sprite.items():
                 if self.individual_sprite_positions[i][0] // self.cell_size == x and self.individual_sprite_positions[i][1] // self.cell_size >= y and self.individual_sprite_positions[i][1] // self.cell_size < self.current_empty_cell[1]:
                     self.individual_sprite_positions[i][1] += self.cell_size
                 i += 1
             self.current_empty_cell = x, y
         elif self.check_right(x, y):
-            i = 1
+            i = 0
             for pos, sprite in self.individual_sprite.items():
                 if self.individual_sprite_positions[i][1] // self.cell_size == y and self.individual_sprite_positions[i][0] // self.cell_size >= x and self.individual_sprite_positions[i][0] // self.cell_size < self.current_empty_cell[0]:
                     self.individual_sprite_positions[i][0] += self.cell_size
                 i += 1
             self.current_empty_cell = x, y
 
+    def randomize(self, level: int = 1000):
+        for _ in range(level): self.handle_sliding((random.randint(0, self.image_size[0]), random.randint(0, self.image_size[1])))
+
+    def is_solved(self):
+        i = 0
+        for pos, _ in self.individual_sprite.items():
+            if pos != tuple(self.individual_sprite_positions[i]): return False
+            i += 1
+        return True
+
 def main():
 
-    puzzle = Puzzle("images/charizard.png")
-
+    puzzle = Puzzle("images/charizard.png", cell_size=400)
+    puzzle.randomize()
     while True:
         events = pg.event.get()
         mouse = pg.mouse.get_pos()
@@ -97,6 +108,7 @@ def main():
 
             elif event.type == pg.MOUSEBUTTONDOWN:
                 puzzle.handle_sliding(mouse)
+                print(puzzle.is_solved())
 
         scr.fill((0, 0, 0))
         puzzle.render(scr)
